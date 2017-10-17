693b21db4ef29b5b25b1e5d2a99df0db439c0464
Francisco Carrera <fc1159@nyu.edu>
2017-10-17 10:25:12 -0400

    Added more css

--- server.js
+++ server.js
@@ -30,29 +30,3 @@ app.use(htmlRoutes);
 app.listen(PORT, function () {
     console.log("Server Listening on Port: " + PORT);
 });
-
-// const request = require('request');
-// const cheerio = require('cheerio');
-
-// request('https://www.reddit.com/r/DestinyTheGame/', function (error, response, body) {
-//     console.log('error:', error); // Print the error if one occurred
-//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//     // console.log('body:', body); // Print the HTML for the Google homepage.
-
-//     var $ = cheerio.load(body);
-
-//     var results = [];
-
-//     $("a.title").each(function (i, element) {
-
-//         var title = $(element).text();
-//         var link = $(element).attr("href");
-
-//         results.push({
-//             title: title,
-//             link: link
-//         });
-//     });
-
-//     console.log(results);
-// });
