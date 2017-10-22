08cf9b5e0a5d072c97d2db51c2e3a4ec951b3350
Francisco Carrera <franciscocarrer@gmail.com>
2017-10-22 01:05:26 -0400

    Added new routes to get comments saved to posts. Posts are sent to front end. Comments can be grabbed in the front end

--- public/js/app.js
+++ public/js/app.js
@@ -27,8 +27,35 @@ $(document).ready(function () {
         });
     });
 
-    $('#exampleModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-      })
+    $('#comment-save').on("click", function (e) {
+        var id = $(this).data('id');
+
+        console.log("Clicked Save Comment Button");
+        $.ajax({
+            method: "POST",
+            url: "/comment/" + id,
+            data: {
+                body: $('#commentArea').val(),
+            }
+        }).done(function (data) {
+            console.log(data);
+        });
+    });
+
+    $('.note-btn').on("click", function (res, req){
+
+        var id = $(this).data('id');
+
+        $.ajax({
+            method: "GET",
+            url: "/comment/" + id,
+        }).done(function (data) {
+            console.log(data);
+        });
+
+        $('#comment-save').data('id', $(this).data('id'));
+        console.log($('#comment-save').data('id'));
+
+    });
     
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -55,6 +55,32 @@ router.get("/saved", function (req, res) {
         });
 });
 
+router.get("/allPosts", function (req, res) {
+    
+    db.Post
+        .find({})
+        .then( function(found) {
+        // Log any errors if the server encounters one
+            res.json(found);
+        })
+        .catch(function (derp) {
+            res.json(derp);
+        });
+});
+
+router.get("/allNotes", function (req, res) {
+    
+    db.Note
+        .find({})
+        .then( function(found) {
+        // Log any errors if the server encounters one
+            res.json(found);
+        })
+        .catch(function (derp) {
+            res.json(derp);
+        });
+});
+
 router.post("/save", function (req, res) {
     console.log("Save body", req.body);
     db.Post
@@ -77,6 +103,49 @@ router.post("/removePost", function (req, res) {
         .catch(function (derp) {
             res.json(derp);
         });
+});
+
+router.get("/comment/:id", function(req, res) {
+
+    db.Post
+      .findOne({ _id: req.params.id })
+      .populate("notes")
+      .then(function(dbPost) {
+        res.json(dbPost);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+router.post("/comment/:id", function(req, res){
+    console.log("Saving Note", req.body);
+    
+    db.Note
+    .create(req.body)
+    .then(function(dbNote) {
+        console.log("Post Id", req.params.id);
+        console.log("Note Reference", dbNote._id);
+        Post.update( { _id : req.params.id } , {$push: { notes: dbNote._id}});
+        // db.Post.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, {new: true});
+    })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+}); 
+
+router.get("/comments", function(req, res){
+    console.log("Getting comments for", req.body);
+    
+});
+
+router.post("/removeComment", function(req, res){
+    console.log("Removing Comment", req.body);
+
 });
 
 module.exports = router;
